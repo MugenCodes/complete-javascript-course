af567e623c0699bb4b981176ed62a7682c0dacff
viewtifult <tashanduncan1994@gmail.com>
2020-08-22 06:49:08 +0100

    Edit coding errors

--- 2-JS-basics/starter/Coding Challenge 1.js
+++ 2-JS-basics/starter/Coding Challenge 1.js
@@ -3,9 +3,9 @@ const markMass = 100;
 const markHeight = 1.9;
 
 const johnMass = 77;
-const johnHeight = 180
+const johnHeight = 1.8;
 
-const calcBMI = (mass, height) => Math.floor(mass / (height * 2));
+const calcBMI = (mass, height) => Math.floor(mass / (height * height));
 const markBMI = calcBMI(markMass,markHeight);
 const johnBMI = calcBMI(johnMass,johnHeight);
 
